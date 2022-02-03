0f968402f2a9057cb51361e5a6ac607e91bf0a1a
Hiemswolf <claytonkruse08@gmail.com>
2022-02-02 21:09:28 -0600

    ricky

--- src/app.js
+++ src/app.js
@@ -6,6 +6,12 @@ const fs = require('fs');
 
 const imgNames = fs.readdirSync(path.join(__dirname, '/images'));
 app.get('/*', (req, res) => {
+
+	if (Math.floor(Math.random() * 50) == 0) {
+		res.redirect('https://www.youtube.com/watch?v=dQw4w9WgXcQ');
+		return;
+	}
+
 	const imgName = imgNames[Math.floor(Math.random() * imgNames.length)];
 	res.sendFile(path.join(__dirname, `/images/${imgName}`));
 });
